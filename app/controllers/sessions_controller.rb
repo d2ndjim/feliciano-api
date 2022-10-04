9a15d20261ae3144b6d6e6a9f2b091bdeb530ed2
Lekan <jimoridwanlekan@gmail.com>
2022-10-04 18:09:04 +0100

    Test authentication endpoint

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
-def create
+class SessionsController < ApplicationController
+  def create
     user = User.find_by_email(session_params[:email])
     if user&.authenticate(session_params[:password])
       token = issue_token(user)
@@ -21,3 +22,4 @@ def create
   def session_params
     params.permit(:email, :password)
   end
+end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :role, :password_digest
+  attributes :id, :first_name, :last_name, :email, :role
 end
