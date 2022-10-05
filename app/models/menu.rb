d10e165c041b6e01ab59c9ab61e0e2c93ef31faa
Lekan <jimoridwanlekan@gmail.com>
2022-10-05 17:05:20 +0100

    Add validation to models

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,9 @@
 class Menu < ApplicationRecord
+  has_many :orders, dependent: :destroy
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 end
