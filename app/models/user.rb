d6cf3efff4e87ab9f1ed1beb5612a86b5d784565
Lekan <jimoridwanlekan@gmail.com>
2022-10-08 18:55:56 +0100

    Add create action and route for menu

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :orders, dependent: :destroy
+  has_many :menus, foreign_key: :admin_id, dependent: :destroy
 
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :orders
-  resources :menus
+  resources :menus, only: [:index, :create]
+
+  #public routes
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   get "/authorized", to: "sessions#show"
+
+  #admin routes
+  # get "admin/menus" => "menus#index"
+  #users routes
 end
