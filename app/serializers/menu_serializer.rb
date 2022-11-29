7f00c89bae6c086480803d5efffbc1de8e43d3ec
Lekan <jimoridwanlekan@gmail.com>
2022-11-29 15:29:08 +0100

    Update menu serializer

--- app/serializers/menu_serializer.rb
+++ app/serializers/menu_serializer.rb
@@ -1,3 +1,3 @@
 class MenuSerializer < ActiveModel::Serializer
-  attributes :id, :name, :category, :price, :description, :image
+  attributes :id, :name, :category, :price, :description, :image, :created_at
 end
