90a8f9d3a8f2b31ae3100b91a9f4877de01b8e65
Lekan <jimoridwanlekan@gmail.com>
2023-01-08 17:06:00 +0100

    Modify menus controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -44,7 +44,33 @@ class MenusController < ApplicationController
     render json: @menus, status: :ok
   end
 
+  def vegetarian
+    @vegetarian = Menu.where(category: 'Vegetarian')
+    render json: @vegetarian, status: :ok
+  end
+
+  def non_vegetarian
+    @non_vegetarian = Menu.where(category: 'Non-Vegetarian')
+    render json: @non_vegetarian, status: :ok
+  end
+
+  def dessert
+    @desserts = Menu.where(category: 'Dessert')
+    render json: @desserts, status: :ok
+  end
+
+  def drink
+    @drinks = Menu.where(category: 'Drinks')
+    render json: @drinks, status: :ok
+  end
+
+  def wine
+    @wines = Menu.where(category: 'Wines')
+    render json: @wines, status: :ok
+  end
+
   private
+  
 
   def menu_params
     params.permit(:name, :category, :price, :description, :image)
